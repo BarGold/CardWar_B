2fde861fb66ba3cae3b0c239c42f0768bc5db901
BarGoldman <93201414+BarGoldman@users.noreply.github.com>
2023-04-08 10:39:25 +0000

    clean Card

--- sources/card.cpp
+++ sources/card.cpp
@@ -1,25 +1,23 @@
 #include "card.hpp"
 
 namespace ariel
-{};
-    // constructor
-    Card::Card(int num, string shape)
-    {
-        this->_num = num;
-        this->_shape = shape;
-    }
-    // // destructor
-    // Card::~Card()
-    // {
-    // }
-    // get the number of Cards
-    int Card::get_NumCard()
-    {
-        return this->_num;
-    }
-    // get the shape of Cards
-    string Card::get_shape()
-    {
-        return this->_shape;
-    }
+{
+};
+// constructor
+Card::Card(int num, string shape)
+{
+    this->_num = num;
+    this->_shape = shape;
+}
 
+// get the number of Cards
+int Card::get_NumCard()
+{
+    return this->_num;
+}
+
+// get the shape of Cards
+string Card::get_shape()
+{
+    return this->_shape;
+}
